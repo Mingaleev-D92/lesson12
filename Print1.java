f93cd39bbbc4abb06e821bf7f7f0fb263a41cd80
Dinar <61611031+Mingaleev-D92@users.noreply.github.com>
2020-07-22 12:55:46 +0300

    Update Print1.java

--- Print1.java
+++ Print1.java
@@ -2,18 +2,7 @@ package ru.innopolis.university.mingaleev.homework_3_12;
 
 public class Print1 {
     public static void putPrint(String[] putPrint) {
-        try{
-            putPrint[6] = "Print";
-            System.out.println(putPrint[6]);
+            String[] strArray = {"java", "IDE", "task"};
+            System.out.println(strArray[3]);
         }
-        catch (ArrayIndexOutOfBoundsException exception){
-            System.out.println("Exceptional situation: " + exception.getMessage());
-        }
-        System.out.println("Normal continuation");
     }
-
-    public static void main(String[] args) {
-        String[] refrigerator = {"Printer", "Suner", "Copier", "Fax"};
-        putPrint(refrigerator);
-    }
-}
