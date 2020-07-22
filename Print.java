2f0dcaf91683914c4665a9b0d3a88cc4d379ab7f
Dinar <61611031+Mingaleev-D92@users.noreply.github.com>
2020-07-22 12:45:24 +0300

    Update Print.java

--- Print.java
+++ Print.java
@@ -1,18 +1,9 @@
 package ru.innopolis.university.mingaleev.homework_3_12;
 
 public class Print {
-    static void printing (){
-        try{
-            System.out.println("Hello, world!");
-            throw new NullPointerException();
-        } catch (NullPointerException e){
-            System.out.println("Exception: " + e.getMessage());
-
-        }
-    }
-
-    public static void main(String[] args) {
-        printing();
+   public static void main(String[] args) {
+        String name = null;
+        System.out.println(name.length());
     }
 }
 
